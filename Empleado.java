9b88967262c953f1f5b1358e0af1e4026f4c0e52
alvaropas17 <alvpastor17@gmail.com>
2025-05-26 10:25:41 +0200

    Getters y setters
    He añadido los getters y los setters para poder acceder desde el sistema de gestión de empleados

--- Empleado.java
+++ Empleado.java
@@ -1,12 +1,23 @@
 class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+    private String nombre;
+    private String cargo;
+    private double salario;
+    
 
     public Empleado(String nombre, String cargo, double salario) {
         this.nombre = nombre;
         this.cargo = cargo;
+        this.setSalario(salario);
+    }
+
+    public double getSalario() {
+        return salario;
+        
+    }
+
+    public void setSalario(double salario) {
         this.salario = salario;
+        
     }
 
     @Override
@@ -14,7 +25,7 @@ class Empleado {
         return "Empleado{" +
                 "nombre='" + nombre + '\'' +
                 ", cargo='" + cargo + '\'' +
-                ", salario=" + salario +
+                ", salario=" + getSalario() +
                 '}';
     }
 }
